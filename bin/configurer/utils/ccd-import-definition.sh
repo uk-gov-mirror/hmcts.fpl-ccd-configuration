f57b050122cb1f1f18934ab904e6e52aa6aebdc8
Tomasz Powroznik <tomas.powroznik@gmail.com>
2020-02-28 15:07:06 +0000

    Test

--- bin/configurer/utils/ccd-import-definition.sh
+++ bin/configurer/utils/ccd-import-definition.sh
@@ -7,6 +7,9 @@ dir=$(dirname ${0})
 userToken=$(${dir}/idam-lease-user-token.sh ${CCD_CONFIGURER_IMPORTER_USERNAME:-ccd.docker.default@hmcts.net} ${CCD_CONFIGURER_IMPORTER_PASSWORD:-Password12})
 serviceToken=$(${dir}/idam-lease-service-token.sh ccd_gw $(docker run --rm toolbelt/oathtool --totp -b ${CCD_API_GATEWAY_S2S_SECRET:-AAAAAAAAAAAAAAAC}))
 
+echo 'userToken ${userToken}'
+echo 'serviceToken ${serviceToken}'
+
 response=$(curl --insecure --silent --show-error -X POST \
   ${CCD_DEFINITION_STORE_API_BASE_URL:-http://localhost:4451}/import \
   -H "Authorization: Bearer ${userToken}" \
--- bin/variables/frontend-load-preview-environment-variables.sh
+++ bin/variables/frontend-load-preview-environment-variables.sh
@@ -10,7 +10,9 @@ echo 'export ENVIRONMENT=preview'
 echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
 echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
 echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
-echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://definition-store-api-adoption-backend-pr-${pr}.service.core-compute-preview.internal"
+echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://definition-store-api-adoption-frontend-pr-${pr}.service.core-compute-preview.internal"
 
 # definition placeholders
-echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://adoption-backend-pr-${pr}.service.core-compute-preview.internal"
+echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://adoption-frontend-pr-${pr}.service.core-compute-preview.internal"
+echo "export BACKEND_IMAGE=pr-16"
+echo "export DEFINITION_BRANCH=pr-16"
