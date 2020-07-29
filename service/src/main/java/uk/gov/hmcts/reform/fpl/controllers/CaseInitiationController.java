f5243e925bd23e899ae795c5218bf8468605650e
Lewis Birks <lewisb@kainos.com>
2020-07-29 15:41:45 +0100

    reproducable working config

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/CaseInitiationController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/CaseInitiationController.java
@@ -3,17 +3,12 @@ package uk.gov.hmcts.reform.fpl.controllers;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.fpl.events.CaseDataChanged;
-import uk.gov.hmcts.reform.fpl.service.LocalAuthorityService;
-import uk.gov.hmcts.reform.fpl.service.LocalAuthorityUserService;
 
 import java.util.Map;
 
@@ -22,32 +17,15 @@ import java.util.Map;
 @RequestMapping("/callback/case-initiation")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class CaseInitiationController {
-    private final LocalAuthorityService localAuthorityNameService;
-    private final LocalAuthorityUserService localAuthorityUserService;
-    private final ApplicationEventPublisher applicationEventPublisher;
 
-    @PostMapping("/about-to-submit")
-    public AboutToStartOrSubmitCallbackResponse handleAboutToSubmitEvent(
-        @RequestBody CallbackRequest callbackrequest) {
-        String caseLocalAuthority = localAuthorityNameService.getLocalAuthorityCode();
-        CaseDetails caseDetails = callbackrequest.getCaseDetails();
+    @PostMapping("/mid-event")
+    public AboutToStartOrSubmitCallbackResponse handleMidEvent(@RequestBody CallbackRequest request) {
+        Map<String, Object> data = request.getCaseDetails().getData();
 
-        Map<String, Object> data = caseDetails.getData();
-        data.put("caseLocalAuthority", caseLocalAuthority);
+        data.remove("caseDocument");
 
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(data)
             .build();
-    }
-
-    @PostMapping("/submitted")
-    public void handleSubmittedEvent(@RequestBody CallbackRequest callbackRequest) {
-        CaseDetails caseDetails = callbackRequest.getCaseDetails();
-        String caseId = Long.toString(caseDetails.getId());
-        String caseLocalAuthority = (String) caseDetails.getData()
-            .get("caseLocalAuthority");
-
-        localAuthorityUserService.grantUserAccessWithCaseRole(caseId, caseLocalAuthority);
-        applicationEventPublisher.publishEvent(new CaseDataChanged(callbackRequest));
     }
 }
