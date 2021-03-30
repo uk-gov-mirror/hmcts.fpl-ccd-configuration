d3a6bd7935f71455e3d98214b11d903a26ff672d
Toireasa Moley <toireasa.moley@hmcts.net>
2021-03-30 17:20:46 +0100

    Adding helper

---
+++ e2e/helpers/tab_assertion_helper.js
@@ -0,0 +1,30 @@
+
+
+function organisationTabFieldSelector(pathToField) {
+  let path = [].concat(pathToField);
+  let fieldName = path.splice(-1, 1)[0];
+  let selector = '//mat-tab-body';
+
+  path.forEach(step => {
+    selector = `${selector}//*[@class="complex-panel" and .//*[@class="complex-panel-title" and .//*[text()="${step}"]]]`;
+  }, this);
+
+  return `${selector}//*[contains(@class,"complex-panel-compound-field") and ..//*[text()="${fieldName}:"]]`;
+}
+
+function seeOrganisationInTab(I, pathToField, fieldValue) {
+  const fieldSelector = this.organisationTabFieldSelector(pathToField);
+
+  if (Array.isArray(fieldValue)) {
+    fieldValue.forEach((value, index) => {
+      I.seeElement(locate(`${fieldSelector}//tr[${index + 1}]`).withText(value));
+    });
+  } else {
+    I.seeElement(locate(fieldSelector).withText(fieldValue));
+  }
+}
+
+module.exports = {
+  seeOrganisationInTab,
+  organisationTabFieldSelector
+};
