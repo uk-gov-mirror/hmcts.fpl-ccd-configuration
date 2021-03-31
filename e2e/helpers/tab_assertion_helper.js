5bff0011ce8d7d6b00fe9ffb5f43f37daed4da13
Toireasa Moley <toireasa.moley@hmcts.net>
2021-03-31 14:53:52 +0100

    Adding the helper reference to laSubmitsApplication

--- e2e/helpers/tab_assertion_helper.js
+++ e2e/helpers/tab_assertion_helper.js
@@ -1,4 +1,31 @@
+function tabFieldSelector(pathToField) {
+  let path = [].concat(pathToField);
+  let fieldName = path.splice(-1, 1)[0];
+  let selector = '//mat-tab-body';
 
+  // if it is a simple case field then it will not have a complex-panel-[title|simple-field] class
+  if (path.length === 0) {
+    return `${selector}//tr[.//th/div[text()="${fieldName}"]]`;
+  }
+
+  path.forEach(step => {
+    selector = `${selector}//*[@class="complex-panel" and .//*[@class="complex-panel-title" and .//*[text()="${step}"]]]`;
+  }, this);
+
+  return `${selector}//*[contains(@class,"complex-panel-simple-field") and .//th/span[text()="${fieldName}"]]`;
+}
+
+function seeInTab(I, pathToField, fieldValue) {
+  const fieldSelector = tabFieldSelector(pathToField);
+
+  if (Array.isArray(fieldValue)) {
+    fieldValue.forEach((value, index) => {
+      I.seeElement(locate(`${fieldSelector}//tr[${index + 1}]`).withText(value));
+    });
+  } else {
+    I.seeElement(locate(fieldSelector).withText(fieldValue));
+  }
+}
 
 function organisationTabFieldSelector(pathToField) {
   let path = [].concat(pathToField);
@@ -25,6 +52,7 @@ function seeOrganisationInTab(I, pathToField, fieldValue) {
 }
 
 module.exports = {
+  seeInTab,
   seeOrganisationInTab,
   organisationTabFieldSelector,
 };
